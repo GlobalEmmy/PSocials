1511fe718b1bf65a01d78e50413ac0bbc18cb5cc
Monyem Emmanuel Onuwa <Emmanuel.Monyem@saipem.com>
2023-09-08 14:30:23 +0100

    complete the first version

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const theme = document.querySelector("#theme");
 const themeModal = document.querySelector(".customize-theme");
 const fontSizes = document.querySelectorAll(".choose-size span");
 var root = document.querySelector(":root");
+const colorPalette = document.querySelectorAll(".choose-color span");
+const bg1 = document.querySelector(".bg-1");
+const bg2 = document.querySelector(".bg-2");
+const bg3 = document.querySelector(".bg-3");
 
 // ======== Notification ==============
 // Remove active class from all menu items
@@ -97,27 +101,109 @@ fontSizes.forEach((size) => {
     size.classList.toggle("active");
     if (size.classList.contains("font-size-1")) {
       fontSize = "10px";
-      root.stlye.setProperty("----sticky-top-left", "5.4rem");
-      root.stlye.setProperty("----sticky-top-right", "5.4rem");
+      root.style.setProperty("----sticky-top-left", "5.4rem");
+      root.style.setProperty("----sticky-top-right", "5.4rem");
     } else if (size.classList.contains("font-size-2")) {
       fontSize = "13px";
-      root.stlye.setProperty("----sticky-top-left", "5.4rem");
-      root.stlye.setProperty("----sticky-top-right", "-7rem");
+      root.style.setProperty("----sticky-top-left", "5.4rem");
+      root.style.setProperty("----sticky-top-right", "-7rem");
     } else if (size.classList.contains("font-size-3")) {
       fontSize = "16px";
-      root.stlye.setProperty("----sticky-top-left", "-2rem");
-      root.stlye.setProperty("----sticky-top-right", "-17rem");
+      root.style.setProperty("----sticky-top-left", "-2rem");
+      root.style.setProperty("----sticky-top-right", "-17rem");
     } else if (size.classList.contains("font-size-4")) {
       fontSize = "19px";
-      root.stlye.setProperty("----sticky-top-left", "-5rem");
-      root.stlye.setProperty("----sticky-top-right", "-25rem");
+      root.style.setProperty("----sticky-top-left", "-5rem");
+      root.style.setProperty("----sticky-top-right", "-25rem");
     } else if (size.classList.contains("font-size-5")) {
       fontSize = "22px";
-      root.stlye.setProperty("----sticky-top-left", "-12rem");
-      root.stlye.setProperty("----sticky-top-right", "-35rem");
+      root.style.setProperty("----sticky-top-left", "-12rem");
+      root.style.setProperty("----sticky-top-right", "-35rem");
     }
 
     //change font size of the root html element
     document.querySelector("html").style.fontSize = fontSize;
   });
+});
+
+// remove active class from colors
+const changeActiveColorClass = () => {
+  colorPalette.forEach((colorPicker) => {
+    colorPicker.classList.remove("active");
+  });
+};
+// change primary colors
+
+colorPalette.forEach((color) => {
+  color.addEventListener("click", () => {
+    let primaryHue;
+    // remove active class from colors
+    changeActiveColorClass();
+
+    if (color.classList.contains("color-1")) {
+      primaryHue = 252;
+    } else if (color.classList.contains("color-2")) {
+      primaryHue = 52;
+    } else if (color.classList.contains("color-3")) {
+      primaryHue = 352;
+    } else if (color.classList.contains("color-4")) {
+      primaryHue = 152;
+    } else if (color.classList.contains("color-5")) {
+      primaryHue = 202;
+    }
+    color.classList.add("active");
+    root.style.setProperty("--primary-color-hue", primaryHue);
+  });
+});
+
+//Theme background values
+
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+//changes background colors
+
+const changeBG = () => {
+  root.style.setProperty("--light-color-lightness", lightColorLightness);
+  root.style.setProperty("--white-color-lightness", whiteColorLightness);
+  root.style.setProperty("--dark-color-lightness", darkColorLightness);
+};
+bg1.addEventListener("click", () => {
+  //Add active class
+  bg1.classList.add("active");
+
+  //Remove active class from others
+  bg2.classList.remove("active");
+  bg3.classList.remove("active");
+  //Remove customised changes from local storage
+  window.location.reload();
+});
+
+bg2.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  whiteColorLightness = "20%";
+  lightColorLightness = "15%";
+
+  //Add active class
+  bg2.classList.add("active");
+
+  //Remove active class from others
+  bg1.classList.remove("active");
+  bg3.classList.remove("active");
+  changeBG();
+});
+
+bg3.addEventListener("click", () => {
+  darkColorLightness = "95%";
+  whiteColorLightness = "10%";
+  lightColorLightness = "0%";
+
+  //Add active class
+  bg3.classList.add("active");
+
+  //Remove active class from others
+  bg1.classList.remove("active");
+  bg2.classList.remove("active");
+  changeBG();
 });
